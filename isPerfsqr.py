960ec467ef23321cdf1a5c0b4c59128b79d749ca
Blu-FiBEr <79830600+Blu-FiBEr@users.noreply.github.com>
2022-11-24 22:53:48 +0530

    Update isPerfsqr.py

--- isPerfsqr.py
+++ isPerfsqr.py
@@ -2,8 +2,8 @@
 
 def isSqr(n):    
     i = 1
-    while(i*i<=n)  
-        if ((n % i == 0) and (n / i == i)):
-            return True    
-        i = i + 1
-    return False
+    while(i * i<= n):
+        if ((n % i == 0) and (n / i == i)):
+            return True
+        i = i + 1
+    return False
